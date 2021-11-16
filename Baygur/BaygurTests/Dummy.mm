0c02a6bede671f0ec8591d2c956e3271e6be49a1
Maxim Grabarnik <maxim@lightricks.com>
2021-11-16 09:32:54 +0200

    BeygurTests: replace objc spec with swift one.

--- Baygur/BaygurTests/Dummy.mm
+++ Baygur/BaygurTests/Dummy.mm
@@ -1,6 +1,4 @@
 // Copyright (c) 2021 Lightricks. All rights reserved.
 // Created by Maxim Grabarnik.
 
-#import <Spectacle/Spectacle-Bridging-Header.h>
-
-#import "Baygur-Bridging-Header.h"
+// This file is required to link with stdlib.
